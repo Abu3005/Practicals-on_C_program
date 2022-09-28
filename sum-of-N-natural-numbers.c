e06233de1238d6f5b7fa73718f989e281b4f71ae
Adetona Mustapha Abodunrin <adetona3005@gmail.com>
2022-09-28 15:12:22 +0000

    sum

---
+++ sum-of-N-natural-numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * The program accept value N and prints the sum of N natural numbers
+ * Natural numbers are 0, 1, 2, 3,.......N
+ */
+
+int main()
+{
+	int N, i, sum = 0;
+
+	printf("Enter number of integers : ");
+	scanf("%d", &N);
+	printf("Sum of %d iS :", N);
+	for (i = 1; i <= N; i++)
+		sum += i;
+	printf("%d\n", sum);
+	return 0;
+}
